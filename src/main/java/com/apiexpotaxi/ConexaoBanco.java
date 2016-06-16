7fb469e3cba5f03a8d686fba99934dfaa4067aa5
Bruno Brito <bruno.brito.ln@gmail.com>
2016-06-16 11:43:03 -0300

    conexão com banco

---
+++ src/main/java/com/apiexpotaxi/ConexaoBanco.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apiexpotaxi;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author bruno
+ */
+public class ConexaoBanco {
+
+    private static Connection getConnection() { //throws URISyntaxException, SQLException {
+        try {
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            return DriverManager.getConnection(dbUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+
+}
